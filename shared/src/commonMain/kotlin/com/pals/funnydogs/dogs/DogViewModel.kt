60131dfc0442d6935b754b4c3e2062f69759e7b3
kalpen <androidpearldeveloper@gmail.com>
2025-03-29 12:30:09 +0530

    @Kalpen Vaghela
    First Initial Commit.

--- shared/src/commonMain/kotlin/com/pals/funnydogs/dogs/DogViewModel.kt
+++ shared/src/commonMain/kotlin/com/pals/funnydogs/dogs/DogViewModel.kt
@@ -1,21 +1,17 @@
 package com.pals.funnydogs.dogs
 
 import com.pals.funnydogs.BaseViewModel
-import io.ktor.client.HttpClient
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.serialization.json.Json
 
 class DogViewModel(
     private val dogUseCase:DogUseCase
 
 ): BaseViewModel() {
 
-    private val _dogState:MutableStateFlow<DogState> = MutableStateFlow(DogState(loading = true))
+    private val _dogState: MutableStateFlow<DogState> = MutableStateFlow(DogState(loading = true))
     val dogState:StateFlow<DogState> get() = _dogState
 
     init {
